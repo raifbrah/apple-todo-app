bf4b42983eece589dcf6a03bed86987865cd17b2
raifbrah <raifbrah@gmail.com>
2022-12-12 21:35:07 +0300

    Отключил zoom

--- main.js
+++ main.js
@@ -19,3 +19,39 @@ document.markJS = markJS;
 document.outgoingWrapper__filesContainerJS = outgoingWrapper__filesContainerJS;
 document.saveToLocalstorageJS = saveToLocalstorageJS;
 document.imgViewerJS = imgViewerJS;
+
+// отключение zoom через скролл (в том числе трекападами в macOS)
+document.addEventListener(
+  "mousewheel",
+  function (e) {
+    if (!e.ctrlKey && !e.metaKey) return;
+
+    e.preventDefault();
+    e.stopImmediatePropagation();
+  },
+  { passive: false }
+);
+
+// отключение zoom прикосновениями (в том числе трекападами и т.п.) в Safari и iOS
+document.addEventListener(
+  "gesturestart",
+  function (e) {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+  },
+  { passive: false }
+);
+
+// отключение zoom через клавиатуру (ctrl + "+", ctrl + "-")
+// кнопки браузера для управления zoom отключены не будут
+document.addEventListener(
+  "keydown",
+  function (e) {
+    if (!e.ctrlKey && !e.metaKey) return;
+    if (e.keyCode != 189 && e.keyCode != 187) return;
+
+    e.preventDefault();
+    e.stopImmediatePropagation();
+  },
+  { passive: false }
+);
